8f07bbfc52a4df27d4fe8e5e4f500cc9aa7e8e80
neerav gupta <neeravgupta07@gmail.com>
2022-11-23 16:27:47 +0530

    modified:   final.py

--- final.py
+++ final.py
@@ -1,5 +1,6 @@
 from pydub import AudioSegment
 from pydub.playback import play
+from gpiozero import LED, Button, RotaryEncoder
 
 # rec numbers meanings
 # 0 = empty
@@ -7,3 +8,16 @@ from pydub.playback import play
 # 2 = playing
 # 3 = muted
 
+rotor = RotaryEncoder(16, 20, wrap=True)
+
+beat_time = 4
+beat_tempo = 120
+
+def set_timeperiod():
+    beat_tempo = rotor.steps
+    beat_time = beat_tempo/15
+    print("set the bet time to "+ beat_tempo +" and the beat tempo to "+ beat_tempo)
+    
+
+rotor.when_rotated = set_timeperiod
+    
